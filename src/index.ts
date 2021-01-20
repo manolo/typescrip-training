2cc3de3ffb559e6ac93f24a18b30f3aed64a38a0
Manolo Carrasco <manolo@vaadin.com>
2021-01-20 08:11:25 +0100

    implement TS homework, module II

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,62 @@
-import _ from "lodash"
+const url = 'https://bad-api-assignment.reaktor.com';
+const categories = ['jackets', 'shirts', 'accessories'];
+let fields: string[];
 
-// That's an example dependency over there ^
+const table = document.createElement('table');
+const body = table.createTBody();
+document.body.appendChild(table);
+interface Product {
+  id: string;
+  availability: string;
+  manufacturer: string;
+}
+interface Availability {
+  id: string;
+  DATAPAYLOAD: any;
+}
 
-console.log("Hello from TypeScript");
-console.log("Make some changes here and see if the watch->compile->reload cycle works");
+async function fetchWithRtry(url: string, ms = 500): Promise<Response> {
+    return fetch(url).catch(error => new Promise((resolve) => setTimeout(() => resolve(fetchWithRtry(url, ms)), ms)));
+}
+
+const tpl: HTMLTemplateElement = document.createElement('template');
+function parseAva(payload: string): string {
+  tpl.innerHTML = payload;
+  return tpl.content.querySelector('instockvalue')?.textContent || '';
+}
+
+async function fetchManufacturer(manufacturer: string) : Promise<any> {
+  const avas = (await (await fetchWithRtry(`${url}/availability/${manufacturer}`)).json()).response as Availability[];
+  if (!(avas instanceof Array)) { // retry, because some times returns a non array
+    return fetchManufacturer(manufacturer);
+  }
+  avas.filter(a => a.id).forEach(a => {
+    const row = document.getElementById(a.id.toLowerCase()) as HTMLTableRowElement;
+    if (row && row.lastElementChild) { // set availability text
+      row.lastElementChild.textContent = parseAva(a.DATAPAYLOAD);
+    }
+  });
+}
+
+async function fetchProduct(product: string): Promise<Product[]> {
+  const prods = (await (await fetchWithRtry(`${url}/products/${product}`)).json()) as Product[];
+  if (!fields) { // create table headers
+    fields = [...Object.keys(prods[0]), 'availability'];
+    const tr = table.createTHead().insertRow();
+    fields.forEach(f => tr.insertCell().textContent = f);
+  }
+  prods.forEach((p) => { // add content to the table
+    const row = body.insertRow();
+    fields.forEach(f => row.insertCell().textContent = (p as any)[f]);
+    row.id = p.id;
+  });
+  return prods;
+}
+
+async function main() {
+  const products = (await Promise.all(categories.map(fetchProduct))).flat();
+  const manufacturers = Array.from(new Set(products.map((prd) => prd.manufacturer)));
+  await Promise.all(manufacturers.map(fetchManufacturer))
+}
+
+main();
