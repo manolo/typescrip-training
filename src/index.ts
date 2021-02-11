b2869b1af74b2404959313da294566a9ec9779c9
Manolo Carrasco <manolo@vaadin.com>
2021-02-11 21:18:38 +0100

    session 3 step 2-3: use io-ts for validating json responses

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as t from 'io-ts';
+import * as Either from "fp-ts/lib/Either"
+import {PathReporter} from 'io-ts/PathReporter'
+
 const url = 'https://bad-api-assignment.reaktor.com';
 const categories = ['jackets', 'shirts', 'accessories'];
 let fields: string[];
@@ -5,18 +9,38 @@ let fields: string[];
 const table = document.createElement('table');
 const body = table.createTBody();
 document.body.appendChild(table);
-interface Product {
-  id: string;
-  availability: string;
-  manufacturer: string;
-}
-interface Availability {
-  id: string;
-  DATAPAYLOAD: any;
-}
+
+const AvailabilityCodec = t.type({
+  id: t.string,
+  DATAPAYLOAD: t.string
+}, "Availability")
+type Availability = t.TypeOf<typeof AvailabilityCodec>
+
+const AvailabilityResponseCodec = t.type({
+  response: t.array(AvailabilityCodec)
+}, "Availability")
+type AvailabilityResponse = t.TypeOf<typeof AvailabilityResponseCodec>
+
+const ProductCodec = t.type({
+  id: t.string,
+  name: t.string,
+  type: t.string,
+  manufacturer: t.string,
+  price: t.number
+}, "Product");
+type Product = t.TypeOf<typeof ProductCodec>
 
 async function fetchWithRtry(url: string, ms = 500): Promise<Response> {
     return fetch(url).catch(error => new Promise((resolve) => setTimeout(() => resolve(fetchWithRtry(url, ms)), ms)));
+}
+
+async function fetchJSON<T>(url: string, codec: t.Type<T>): Promise<T> {
+  const result = codec.decode(await (await fetchWithRtry(url)).json());
+  if (Either.isRight(result)) {
+      return result.right
+  } else {
+      throw Error(PathReporter.report(result).join("\n"))
+  }
 }
 
 function parseAva(payload: string): string {
@@ -24,10 +48,7 @@ function parseAva(payload: string): string {
 }
 
 async function fetchManufacturer(manufacturer: string) : Promise<any> {
-  const avas = (await (await fetchWithRtry(`${url}/availability/${manufacturer}`)).json()).response as Availability[];
-  if (!(avas instanceof Array)) { // retry, because some times returns a non array
-    return fetchManufacturer(manufacturer);
-  }
+  const avas = await (await fetchJSON(`${url}/availability/${manufacturer}`, AvailabilityResponseCodec)).response
   avas.filter(a => a.id).forEach(a => {
     const row = document.getElementById(a.id.toLowerCase()) as HTMLTableRowElement;
     if (row && row.lastElementChild) { // set availability text
@@ -37,7 +58,8 @@ async function fetchManufacturer(manufacturer: string) : Promise<any> {
 }
 
 async function fetchProduct(product: string): Promise<Product[]> {
-  const prods = (await (await fetchWithRtry(`${url}/products/${product}`)).json()) as Product[];
+  const prods = await fetchJSON(`${url}/products/${product}`, t.array(ProductCodec));
+
   if (!fields) { // create table headers
     fields = [...Object.keys(prods[0]), 'availability'];
     const tr = table.createTHead().insertRow();
@@ -54,7 +76,7 @@ async function fetchProduct(product: string): Promise<Product[]> {
 async function main() {
   const products = (await Promise.all(categories.map(fetchProduct))).flat();
   const manufacturers = Array.from(new Set(products.map((prd) => prd.manufacturer)));
-  await Promise.all(manufacturers.map(fetchManufacturer))
+  await Promise.all(manufacturers.map(fetchManufacturer));
 }
 
 main();
